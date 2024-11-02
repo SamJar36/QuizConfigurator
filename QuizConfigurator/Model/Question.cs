70b63693bd6f795df2cb42e97f8cdceb1a24f289
Samuel <samandreasjarva@gmail.com>
2024-11-02 16:09:52 +0100

    Add question, correct and incorrect answers correctly binded to activepack

--- QuizConfigurator/Model/Question.cs
+++ QuizConfigurator/Model/Question.cs
@@ -10,14 +10,17 @@ namespace QuizConfigurator.Model
     {
         public string Query { get; set; }
         public string CorrectAnswer { get; set; }
-        public string IncorrectAnswer1 { get; }
-        public string IncorrectAnswer2 { get; }
-        public string IncorrectAnswer3 { get; }
+        public string IncorrectAnswer1 { get; set; }
+        public string IncorrectAnswer2 { get; set; }
+        public string IncorrectAnswer3 { get; set; }
         public string[] IncorrectAnswers { get; set; }
         public Question(string query, string correctAnswer, string incorrectAnswer1, string incorrectAnswer2, string incorrectAnswer3)
         {
             Query = query;
             CorrectAnswer = correctAnswer;
+            IncorrectAnswer1 = incorrectAnswer1;
+            IncorrectAnswer2 = incorrectAnswer2;
+            IncorrectAnswer3 = incorrectAnswer3;
             IncorrectAnswers = new string[3] { incorrectAnswer1, incorrectAnswer2, incorrectAnswer3 };
         }
     }
--- QuizConfigurator/ViewModel/ConfigurationModel.cs
+++ QuizConfigurator/ViewModel/ConfigurationModel.cs
@@ -1,4 +1,5 @@
 ﻿using QuizConfigurator.Command;
+using QuizConfigurator.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,18 @@ namespace QuizConfigurator.ViewModel
     internal class ConfigurationViewModel : ViewModelBase
     {
         private readonly MainWindowViewModel? mainWindowViewModel;
+
+        private Question _selectedQuestion;
+
+        public Question SelectedQuestion
+        {
+            get => _selectedQuestion;
+            set
+            {
+                _selectedQuestion = value;
+                RaisePropertyChanged();
+            }
+        }
         public string? ActivePackName => mainWindowViewModel?.ActivePack?.Name;
 
         public ConfigurationViewModel(MainWindowViewModel? mainWindowViewModel)
